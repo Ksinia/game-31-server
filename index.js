53a658d8f8348831559a859bca07c50534c5ae23
Ksinia <xiatian@mail.ru>
2019-12-16 11:44:48 +0100

    Add test endpoint

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const express = require("express");
 const port = process.env.PORT || 4000;
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
 app.listen(port, () => console.log(`Listening on port: ${port}`));
