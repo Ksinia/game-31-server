b0bd293ff5b24e3724a528c35b5bc60a2df46314
Ksinia <xiatian@mail.ru>
2019-12-17 14:05:55 +0100

    Add maxPlayer to room model

--- room/model.js
+++ room/model.js
@@ -2,7 +2,14 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const Room = db.define("room", {
-  name: Sequelize.STRING
+  name: Sequelize.STRING,
+  maxPlayers: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 2,
+      max: 4
+    }
+  }
 });
 
 module.exports = Room;
