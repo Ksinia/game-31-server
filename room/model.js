1312dfa344ebf2689abe354a65e81dcdb7484b8b
Svdbroek <s_in_the_water@hotmail.com>
2019-12-19 14:43:43 +0100

    created start endpoint

--- room/model.js
+++ room/model.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
 const User = require("../user/model");
-
+const Card = require("../card/model");
 const Room = db.define("room", {
   name: Sequelize.STRING,
   maxPlayers: {
@@ -10,11 +10,19 @@ const Room = db.define("room", {
       min: 2,
       max: 4
     }
-  }
+  },
+  phase: Sequelize.ENUM("waiting", "ready", "started", "finished"),
+  turn: Sequelize.INTEGER,
+  passed: { type: Sequelize.INTEGER, default: null }
 });
 
 User.belongsTo(Room);
 Room.hasMany(User, {
   allowNull: true
 });
+Card.belongsTo(User);
+Card.belongsTo(Room);
+
+Room.hasMany(Card);
+User.hasMany(Card);
 module.exports = Room;
