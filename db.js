ccaf8131c324d875190057ead74088e8f653924f
Ksinia <xiatian@mail.ru>
2022-12-22 01:45:46 +0100

    Update modules, fix db connection

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
 const databaseUrl =
   process.env.DATABASE_URL ||
-  "postgres://postgres:cheesecake@localhost:5432/postgres";
+  "postgres://postgres:secret@localhost:5432/game31";
 const db = new Sequelize(databaseUrl, { logging: false }); // to get rid of so many messages in console
 
 db.sync({ force: false }) // dont delete data on sync
